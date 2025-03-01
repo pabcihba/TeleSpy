4b6d136ffabae9eec19a3d7ab70b18d0b020330e
Pabcihba <pabcihba@gmail.com>
2025-03-01 10:07:59 +0300

    Добавил скрипт для удаления JSON файлов

---
+++ reboot.sh
@@ -0,0 +1,18 @@
+clear
+echo Через 5 секунд будут удалены JSON файлы , отвечающие за список прослушиваемых пользователей и сообщений
+echo CTRL + C , чтобы отменить
+sleep 1s
+echo 4
+sleep 1s
+echo 3
+sleep 1s
+echo 2
+sleep 1s
+echo 1
+sleep 1s
+clear
+rm messages_cache.json
+rm spy_list.json
+echo Произведено удаление JSON файлов
+echo Попробуй опять поставить на кого нибудь прослушку и
+echo проверить работоспособность скрипта
